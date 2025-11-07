fe9b1f950d693c738f4b457337f382784b956aec
super-toq <toq@mailbox.org>
2025-11-07 13:02:14 +0100

    update

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A simple demo, nothing more.
 
 ![window](preview-img1.png?raw=true) 
 
-I review code, try to understand it, experiment with it, combine it, and learn from what happens."
+I review code, try to understand it, experiment with it, combine it and learn from what happens...
--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 /*
- * toq 2025, LICENCE MIT
+ * toq 2025, LICENSE: MIT
  *
  * gcc $(pkg-config --cflags gtk4 libadwaita-1) -o window main.c $(pkg-config --libs gtk4 libadwaita-1) 
  */
 
 #include <adwaita.h>
+
 
 static void
 on_activate (AdwApplication *app, gpointer user_data) //Callback-Funktion wird aufgerufen wenn Anw. aktiviert wird
@@ -12,18 +13,25 @@ on_activate (AdwApplication *app, gpointer user_data) //Callback-Funktion wird a
     /* ----- Adwaita-Fenster wird erstellt und als "win" gespeichert -------------------------- */
     AdwApplicationWindow *win = ADW_APPLICATION_WINDOW (adw_application_window_new (GTK_APPLICATION (app))); 
 
-    gtk_window_set_title (GTK_WINDOW (win), "empty window");   // Fenstertitel
-    gtk_window_set_default_size (GTK_WINDOW (win), 640, 360);  // Standard-Fenstergröße
-    gtk_window_present (GTK_WINDOW (win));                     // Fenster anzeigen lassen
+    gtk_window_set_title (GTK_WINDOW(win), "empty window");   // Fenstertitel
+    gtk_window_set_default_size (GTK_WINDOW(win), 640, 400);  // Standard-Fenstergröße
+    gtk_window_present (GTK_WINDOW(win));                     // Fenster anzeigen lassen
 
     /* ----- ToolbarView (als Root‑Widget) erstellt und als Inhalt des Fensters festgelegt ---- */
     AdwToolbarView *toolbar_view = ADW_TOOLBAR_VIEW (adw_toolbar_view_new ());
     adw_application_window_set_content (win, GTK_WIDGET (toolbar_view));
 
     /* ----- HeaderBar mit TitelWidget erstellt und dem ToolbarView hinzugefügt -------------- */
-    AdwHeaderBar *header = ADW_HEADER_BAR (adw_header_bar_new ());
-    adw_header_bar_set_title_widget (header, gtk_label_new ("My first Window"));
+    AdwHeaderBar *header = ADW_HEADER_BAR (adw_header_bar_new());
+    adw_header_bar_set_title_widget (header, gtk_label_new("My First Window"));
     adw_toolbar_view_add_top_bar (toolbar_view, GTK_WIDGET (header));
+
+        /* ----- Text-Label erstellen  ----- */
+    GtkWidget *label = gtk_label_new("~ libadwaita ~");
+
+    /* ----- Label als als Inhalt der AdwApplicationWindow festlegen ----- */ 
+    adw_toolbar_view_set_content (toolbar_view, label);
+
 }
 
 /* ---------------------------------------------------------------------------
@@ -33,9 +41,8 @@ int
 main (int argc, char **argv)
 {
     g_autoptr (AdwApplication) app =                        // Instanz erstellen, mit App-ID und Default-Flags
-        adw_application_new ("free.toq.emptywindow", G_APPLICATION_DEFAULT_FLAGS);
+        adw_application_new ("free.toq.window", G_APPLICATION_DEFAULT_FLAGS);
 
     g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL); // Signal mit on_activate verbinden
 
     return g_application_run (G_APPLICATION (app), argc, argv); // g_application_run startet Anw. u. wartet auf Ereignis
-}
