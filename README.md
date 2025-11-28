043369a9ec5dcabf6485b622a929a10edde25368
super-toq <toq@mailbox.org>
2025-11-28 10:47:38 +0100

    Improve Adwaita-like styling, rename c file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### EmptyWindow is a libadwaita window
+### EmptyWindow is a Gtk4/libadwaita window
 
 A simple demo, nothing more.
 
@@ -6,7 +6,7 @@ I’d like to understand how to create an Adwaita‑styled window entirely in C 
 
 ![window](preview-img1.png?raw=true) 
 
-Yes, I’m currently interested in how applications are built and how the code works; I study source code, try to understand it, experiment with different approaches, and learn from the experience I gain along the way.  
+What for? Now I’m currently interested in how applications are built and how the code works. I study source code, try to understand it, experiment with different approaches, and learn from the experience I gain along the way.  
   
 Personally, I work with the GNOME desktop environment and I like the Adwaita design, which is why I chose it.  
   
---
+++ emptywindow.c
@@ -0,0 +1,124 @@
+/* EMPTYWINDOW is part of a learning project
+ * toq 2025 Nov  LICENSE: MIT
+ * 
+ * 
+ * From my point of view, this codebase is complete so far, I will then turn to the next level.
+ * 
+ * gcc $(pkg-config --cflags libadwaita-1) -o emptywindow emptywindow.c  $(pkg-config --libs libadwaita-1)
+ * 
+ * Beispiel:
+ * adw_application_window_set_content = Adwaita-Funktion;
+ * GTK_WIDGET() Gtk-Macro, konvertiert einen spezifischen Widget-Typ in den allgemeinen GtkWidget-Typ;
+ * AdwToolbarView = Adwaita-Widget
+ *
+ * 
+ * Version 0.4   2025-11-28
+ * 
+ * Please note:
+ * The Use of this code and execution of the applications is at your own risk, I accept no liability!
+ */
+
+#include <adwaita.h>
+
+/* ----- Response des Dialogs prüfen ----- */
+static void on_alert_dialog_response (AdwAlertDialog *dialog, const char *response, gpointer user_data)
+{
+    if (g_strcmp0 (response, "ok") == 0)
+        g_print ("OK pressed\n");
+    else
+        g_print ("Cancel pressed\n");
+}
+
+static void on_button_clicked (GtkButton *button, gpointer user_data)
+{
+    /* ----- Fenster in welchem der Button untergeordnet ist holen ----- */
+    GtkWindow *parent = GTK_WINDOW(gtk_widget_get_root(GTK_WIDGET(button)));
+    /* ----- ist Fenster gültig? ----- */
+    if (!parent || !GTK_IS_WINDOW(parent)) {
+        g_warning("No valid parent window for alert dialog");
+        return;
+    }
+    g_print("Hello my friend\n");
+
+    /* ----- Alert-Dialog erstellen ----- */
+    AdwAlertDialog *dialog = ADW_ALERT_DIALOG(adw_alert_dialog_new("My Message", 
+     "First attempts completed, \nmoving on to the next codebase."));
+    /* ----- Schaltflächen hinzufügen ----- */
+    //adw_alert_dialog_add_response(dialog, "cancel", "Cancel");
+    adw_alert_dialog_add_response(dialog, "ok", "OK");
+    adw_alert_dialog_set_default_response(dialog, "ok");
+
+    /* ----- Antwort-Signal mit Callback verbinden ----- */
+    g_signal_connect(dialog, "response", G_CALLBACK(on_alert_dialog_response), NULL);
+
+    /* ----- Dialog ausgeben ----- */
+    adw_dialog_present(ADW_DIALOG(dialog), GTK_WIDGET(parent));
+}
+
+/* --------------------------------------------------------------------------- */
+/* ----- Callback-Funktion wird aufgerufen wenn Anwendung aktiviert wird ----- */
+static void on_activate (AdwApplication *app, gpointer)
+{
+    /* ----- Adwaita-Fenster wird erstellt und als "win" gespeichert ------------------------ */
+    AdwApplicationWindow *win = ADW_APPLICATION_WINDOW (adw_application_window_new (GTK_APPLICATION (app))); 
+    gtk_window_set_title (GTK_WINDOW(win), "emptywindow");        // der WM-Title
+
+    /* ----- ToolbarView (als Root‑Widget) erstellt und als Inhalt des Fensters festgelegt -- */
+    AdwToolbarView *toolbar_view = ADW_TOOLBAR_VIEW (adw_toolbar_view_new ());
+    adw_application_window_set_content (win, GTK_WIDGET (toolbar_view));
+
+    /* ----- HeaderBar mit TitelWidget erstellt und dem ToolbarView hinzugefügt ------------ */
+    AdwHeaderBar *header = ADW_HEADER_BAR (adw_header_bar_new());
+
+    /* ----- Fenstertitel mit Adwaita .heading Class ------ */
+    GtkWidget * title_label = gtk_label_new ("My First Window");  // Label für Fenstertitel
+    gtk_widget_add_css_class (title_label, "heading");            // .heading class
+    adw_header_bar_set_title_widget ( ADW_HEADER_BAR (header), title_label);
+    gtk_window_set_default_size (GTK_WINDOW(win), 640, 400);      // Fenstergröße
+
+    /* ----- Schaltfläche erzeugen ----- */
+    GtkWidget *first_btn = gtk_button_new_with_label("Press me");
+
+    /* ----- Schaltfläche links in die Headerbar setzen ----- */
+    adw_header_bar_pack_start(header, first_btn);
+
+    /* ----- Schaltfläche in Theme-Akzent-Farbe ----- */
+    gtk_widget_add_css_class (first_btn, "suggested-action");
+
+    /* ----- Headerbar der Toolbarview hinzufügen ----- */
+    adw_toolbar_view_add_top_bar (toolbar_view, GTK_WIDGET (header));
+
+    /* ----- Schalflächen verbinden ----- */
+    g_signal_connect(first_btn, "clicked", G_CALLBACK(on_button_clicked), app);
+
+    /* ---- Haupt‑Box erstellen ----------------------------------------------------------- */
+    GtkBox *box = GTK_BOX (gtk_box_new (GTK_ORIENTATION_VERTICAL, 12));
+    gtk_widget_set_margin_top    (GTK_WIDGET (box), 20);
+    gtk_widget_set_margin_bottom (GTK_WIDGET (box), 20);
+    gtk_widget_set_margin_start  (GTK_WIDGET (box), 20);
+    gtk_widget_set_margin_end    (GTK_WIDGET (box), 20);
+    gtk_widget_set_hexpand (GTK_WIDGET (box), TRUE);
+    gtk_widget_set_vexpand (GTK_WIDGET (box), TRUE);
+
+    /* -----  Box zur ToolbarView hinzufügen ------------ */
+    adw_toolbar_view_set_content(toolbar_view, GTK_WIDGET(box));
+    // hier kann noch mehr hinzugefügt werden...
+
+    /* ----- Fenster desktop‑konform anzeigen lassen ----- */
+    gtk_window_present(GTK_WINDOW(win));
+
+}
+
+/* ---------------------------------------------------------------------------
+ * Anwendungshauptteil, main()
+ * --------------------------------------------------------------------------- */
+int main (int argc, char **argv)
+{
+
+    /* ----- Instanz erstellen + App-ID + Default-Flags ----- */
+    g_autoptr (AdwApplication) app = adw_application_new ("free.toq.window", G_APPLICATION_DEFAULT_FLAGS);
+
+    g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL); // Signal mit on_activate verbinden
+    /* --- g_application_run startet Anwendung u. wartet auf Ereignis --- */
+    return g_application_run (G_APPLICATION (app), argc, argv);
+}
